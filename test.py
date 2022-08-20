383a5455d82cf294833669e7ab1bcac8912dc13e
Bill Mill <bill@billmill.org>
2022-08-20 13:52:27 -0400

    add test.py

---
+++ test.py
@@ -0,0 +1,115 @@
+import berserk
+from datetime import datetime, timezone
+import json
+import os
+import sys
+import time
+
+TOK = os.environ.get("LICHESS_TOKEN")
+
+sess = berserk.TokenSession(TOK)
+client = berserk.Client(sess)
+
+me = client.account.get()
+
+# https://stackoverflow.com/a/22238613/42559
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        return millis_since_epoch(obj)
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+def millis_since_epoch(dt: datetime) -> int:
+    return int(dt.timestamp() * 1000)
+
+
+def datetime_from_seconds(ts):
+    """Return the datetime for the given seconds since the epoch.
+
+    UTC is assumed. The returned datetime is timezone aware.
+
+    :return: timezone aware datetime
+    :rtype: :class:`datetime`
+    """
+    return datetime.fromtimestamp(ts, timezone.utc)
+
+
+def datetime_from_millis(millis):
+    """Return the datetime for the given millis since the epoch.
+
+    UTC is assumed. The returned datetime is timezone aware.
+
+    :return: timezone aware datetime
+    :rtype: :class:`datetime`
+    """
+    return datetime_from_seconds(millis / 1000)
+
+
+if os.path.isfile("raw_games.json"):
+    games = json.load(open("raw_games.json"))
+else:
+    outfile = open("raw_games.json", "w")
+    games = list(
+        client.games.export_by_player(
+            "llimllib",
+            max=60,
+            moves=True,
+            pgn_in_json=True,
+            tags=True,
+            clocks=True,
+            evals=True,
+            # export_by_player does not seem to support the `literate` flag. This is
+            # probably a bug. https://github.com/ZackClements/berserk/pull/24
+            # so I added it and installed my own version of the library
+            literate=True,
+        )
+    )
+    json.dump(games, outfile, default=json_serial)
+
+# The game stream is throttled, depending on who is making the request:
+#
+#     Anonymous request: 20 games per second
+#     OAuth2 authenticated request: 30 games per second
+#     Authenticated, downloading your own games: 60 games per second
+
+# let's download 60 games per second and stuff them into a file - later we can
+# process them intelligently into a database
+while 1:
+    # right now this breaks if the file already exists, because json
+    # deserialization doesn't desrialize the timestamps
+    until = millis_since_epoch(games[-1]["createdAt"])
+    newgames = list(
+        client.games.export_by_player(
+            "llimllib",
+            max=60,
+            until=until,
+            moves=True,
+            pgn_in_json=True,
+            tags=True,
+            clocks=True,
+            evals=True,
+            literate=True,
+        )
+    )
+
+    if not newgames:
+        break
+
+    games.extend(newgames)
+
+    # rewrite our raw games file
+    outfile = open("raw_games.json", "w")
+    json.dump(games, outfile, default=json_serial)
+
+    if len(set(g["id"] for g in games)) != len(games):
+        print(
+            "duplicates found, quitting", len(set(g["id"] for g in games)), len(games)
+        )
+
+    time.sleep(1)
+    sys.stdout.write(".")
+    sys.stdout.flush()
+
+print()
